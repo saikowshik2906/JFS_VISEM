7ca56019b4277ec8c310536bf3ebbdbd40faa511
Chenchu Venkatasai Kowshik kumar Thummala <155244691+saikowshik2906@users.noreply.github.com>
2025-12-01 10:46:37 +0530

    first change

--- Example.java
+++ Example.java
@@ -3,5 +3,6 @@ public class Example
     public static void main(String[] args)
     {
         System.out.println("Welcome to SNB1 batch");
+        System.out.println("Day 1: Practice git");
     }
 }
