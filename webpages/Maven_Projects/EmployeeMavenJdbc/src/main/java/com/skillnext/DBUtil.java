1c7e8884ce61df35ea93cee84101a1c890151954
Chenchu Venkatasai Kowshik kumar Thummala <155244691+saikowshik2906@users.noreply.github.com>
2026-01-08 13:35:08 +0530

    5-jan

---
+++ webpages/Maven_Projects/EmployeeMavenJdbc/src/main/java/com/skillnext/DBUtil.java
@@ -0,0 +1,17 @@
+package com.skillnext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil
+{
+
+    public static Connection getConnection() throws Exception 
+  {
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/skill_db",
+            "root",
+            ""
+        );
+    }
+}
